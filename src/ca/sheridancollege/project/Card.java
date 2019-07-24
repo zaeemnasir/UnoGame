e3f581a4578364112ef0bbb64e6ddc023191e00d
Muhammad Zaeem Nasir <43080089+zaeemnasir@users.noreply.github.com>
2019-07-24 14:04:58 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.project;
  * enough to be instantiated for any Card game. Students wishing to add to the code 
  * should remember to add themselves as a modifier.
  * @author dancye, 2018
+ * @contributer: A-Team (Zaeem Nasir)
  */
 public abstract class Card 
 {
